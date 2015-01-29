86aae781a9f931c34c32b8a3fd016207ac1c9cbb
Jeff Black <jeffblack360@gmail.com>
2015-01-29 17:48:54 -0600

    Complete conversion calculations

--- main.cpp
+++ main.cpp
@@ -11,12 +11,25 @@ using namespace std;
 int main(int argcount, char* args[])
 {
     int celsius;
+    int fahrenheit;
     int factor;
 
-    // Enter the temp in Celsius
     cout << "Enter the temperature in Celsius: ";
     cin >> celsius;
 
-    // Calculate conversion factor for Celsius
+    // Calculate conversion factor for Fahrenheit conversion
+    factor = 212 - 32;
+
+    // Use conversion factor to calculate Fahrenheit
+    fahrenheit = factor * celsius/100 + 32;
+
+    // Output results
+    cout << "Fahrenheit value is: " << fahrenheit << endl;
+
+    // Wait on user...
+    cout << "Press Enter to continue..." << endl;
+    cin.ignore(10, '\n');
+    cin.get();
+
     return 0;
 }
