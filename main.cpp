7e7f9e09ff809165c2869f79913083b3475a018e
Jeff Black <jeffblack360@gmail.com>
2015-01-29 17:36:53 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+    Conversion - Program to convert temperature from Celsius to Fahrenheit:
+    Fahrenheit = Celsius * (212 - 32)/100 + 32
+*/
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argcount, char* args[])
+{
+    int celsius;
+    int factor;
+
+    // Enter the temp in Celsius
+    cout << "Enter the temperature in Celsius: ";
+    cin >> celsius;
+
+    // Calculate conversion factor for Celsius
+    return 0;
+}
